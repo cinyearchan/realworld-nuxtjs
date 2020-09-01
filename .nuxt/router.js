59c17cbfa1ff4f5f5c1fdecc6576e6696872b975
cinyearchan <cinyearchan@gmail.com>
2020-09-01 17:06:57 +0800

    finish pages import

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -6,6 +6,10 @@ import scrollBehavior from './router.scrollBehavior.js'
 const _03e9b924 = () => interopDefault(import('../pages/layout' /* webpackChunkName: "" */))
 const _b7d343ba = () => interopDefault(import('../pages/home' /* webpackChunkName: "" */))
 const _4224a2b6 = () => interopDefault(import('../pages/login' /* webpackChunkName: "" */))
+const _99392c36 = () => interopDefault(import('../pages/profile' /* webpackChunkName: "" */))
+const _4a49e367 = () => interopDefault(import('../pages/settings' /* webpackChunkName: "" */))
+const _1b876c9e = () => interopDefault(import('../pages/editor' /* webpackChunkName: "" */))
+const _cba4a59c = () => interopDefault(import('../pages/article' /* webpackChunkName: "" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -38,6 +42,22 @@ export const routerOptions = {
       path: "/register",
       component: _4224a2b6,
       name: "register"
+    }, {
+      path: "/profile/:username",
+      component: _99392c36,
+      name: "profile"
+    }, {
+      path: "/settings",
+      component: _4a49e367,
+      name: "settings"
+    }, {
+      path: "/editor",
+      component: _1b876c9e,
+      name: "editor"
+    }, {
+      path: "/article/:slug",
+      component: _cba4a59c,
+      name: "article"
     }]
   }],
 
