8e9e8f25a17ed46710c56ae9775464bc9be6c2f9
cinyearchan <cinyearchan@gmail.com>
2020-09-02 22:06:33 +0800

    article meta seo

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -89,6 +89,14 @@ export default {
   },
   components: {
     ArticleMeta
+  },
+  head() {
+    return {
+      title: `${this.article.title} - RealWorld`,
+      meta: [
+        { hid: 'description', name: 'description', content: this.article.description }
+      ]
+    }
   }
 };
 </script>
