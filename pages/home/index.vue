a48f6d6dbe212aa5e5a937ea0a1c9978e7882de1
cinyearchan <cinyearchan@gmail.com>
2020-09-02 17:00:00 +0800

    add tag & improve async request

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -76,14 +76,7 @@
             <p>Popular Tags</p>
 
             <div class="tag-list">
-              <a href class="tag-pill tag-default">programming</a>
-              <a href class="tag-pill tag-default">javascript</a>
-              <a href class="tag-pill tag-default">emberjs</a>
-              <a href class="tag-pill tag-default">angularjs</a>
-              <a href class="tag-pill tag-default">react</a>
-              <a href class="tag-pill tag-default">mean</a>
-              <a href class="tag-pill tag-default">node</a>
-              <a href class="tag-pill tag-default">rails</a>
+              <nuxt-link to v-for="item in tags" :key="item" class="tag-pill tag-default">{{ item }}</nuxt-link>
             </div>
           </div>
         </div>
@@ -94,20 +87,29 @@
 
 <script>
 import { getArticles } from "@/api/article";
+import { getTags } from "@/api/tag";
 
 export default {
   name: "HomeIndex",
   async asyncData({ query }) {
     const page = Number.parseInt(query.page || 1)
     const limit = 2
-    const { data } = await getArticles({
-      limit,
+
+    const [articleRes, tagRes] = await Promise.all([
+      getArticles({
+        limit,
       offset: (page - 1) * limit
-    })
-    // console.log(data)
+      }),
+      getTags()
+    ])
+
+    const { articles, articlesCount } = articleRes.data
+    const { tags } = tagRes.data
+
     return {
-      articles: data.articles,
-      articlesCount: data.articlesCount,
+      articles,
+      articlesCount,
+      tags,
       limit,
       page
     }
