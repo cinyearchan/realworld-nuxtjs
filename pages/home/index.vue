4a2972f4de3b9c7b1c6c8722602be029a11e0953
cinyearchan <cinyearchan@gmail.com>
2020-09-02 16:28:57 +0800

    page params

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -89,7 +89,12 @@ import { getArticles } from "@/api/article";
 export default {
   name: "HomeIndex",
   async asyncData() {
-    const { data } = await getArticles()
+    const page = 3
+    const limit = 2
+    const { data } = await getArticles({
+      limit,
+      offset: (page - 1) * limit
+    })
     // console.log(data)
     return {
       articles: data.articles,
