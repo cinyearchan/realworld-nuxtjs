aee7e87d4206033f80e8651881745ecff6f6aefb
cinyearchan <cinyearchan@gmail.com>
2020-09-03 17:56:12 +0800

    author follow

--- pages/article/components/article-meta.vue
+++ pages/article/components/article-meta.vue
@@ -12,28 +12,56 @@
       <nuxt-link :to="{ name: 'profile', params: { username: article.author.username } }" class="author">{{ article.author.username }}</nuxt-link>
       <span class="date">{{ article.createdAt | date('MMM DD, YYYY') }}</span>
     </div>
-    <button class="btn btn-sm btn-outline-secondary" :class="{ active: article.author.following }">
+    <button class="btn btn-sm btn-outline-secondary" :class="{ active: article.author.following }" @click="onFollow(article)" :disabled="article.followDisabled">
       <i class="ion-plus-round"></i>
       &nbsp;
-      Follow Eric Simons
-      <span class="counter">(10)</span>
+      {{  article.author.following ? 'Unfollow' : 'Follow' }} {{ article.author.username }}
+      <!-- <span class="counter">(10)</span> -->
     </button>
     &nbsp;&nbsp;
-    <button class="btn btn-sm btn-outline-primary" :class="{ active: article.author.favorited }">
+    <button class="btn btn-sm btn-outline-primary" :class="{ active: article.favorited }" @click="onFavorite(article)" :disabled="article.favoriteDisabled">
       <i class="ion-heart"></i>
       &nbsp;
       Favorite Post
-      <span class="counter">(29)</span>
+      <span class="counter">{{ article.favoritesCount }}</span>
     </button>
   </div>
 </template>
 <script>
+import { addFollow, deleteFollow, addFavorite, deleteFavorite } from "@/api/article";
+
 export default {
   name: "ArticleMeta",
   props: {
     article: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    async onFavorite(article) {
+      article.favoriteDisabled = true;
+      if (article.favorited) {
+        await deleteFavorite(article.slug);
+        article.favorited = false;
+        article.favoritesCount += -1;
+      } else {
+        await addFavorite(article.slug);
+        article.favorited = true;
+        article.favoritesCount += 1;
+      }
+      article.favoriteDisabled = false;
+    },
+    async onFollow(article) {
+      article.followDisabled = true;
+      if (article.author.following) {
+        await deleteFollow(article.author.username)
+        article.author.following = false
+      } else {
+        await addFollow(article.author.username)
+        article.author.following = true
+      }
+      article.followDisabled = false;
     }
   }
 };
