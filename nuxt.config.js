96526f85bdc68bec35b365474cad3e350893cbdf
cinyearchan <cinyearchan@gmail.com>
2020-09-03 11:18:30 +0800

    deploy params

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,18 @@
  */
 
 module.exports = {
+  head: {
+    link: [
+      // <!-- Import Ionicon icons & Google Fonts our Bootstrap theme relies on -->
+      { rel: 'stylesheet', type: 'text/css', href: '//cdn.jsdelivr.net/npm/ionicons@2.0.1/css/ionicons.min.css' },
+      // <link href="//cdn.jsdelivr.net/npm/ionicons@2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+      { rel: 'stylesheet', type: 'text/css', href: '//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic' },
+      // <link href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+      // <!-- Import the custom Bootstrap 4 theme from our hosted CDN -->
+      { rel: 'stylesheet', type: 'text/css', href: process.env.DEPLOY_ENV === 'gh-pages' ? '/realworld-nuxtjs/index.css' : '/index.css' }
+      // <link rel="stylesheet" href="{{ process.env.DEPLOY_ENV === 'gh-pages' ? '/realworld-nuxtjs/index.css' : '/index.css' }}"></link>
+    ]
+  },
   router: {
     base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/realworld-nuxtjs/' : '/',
     linkActiveClass: 'active',
